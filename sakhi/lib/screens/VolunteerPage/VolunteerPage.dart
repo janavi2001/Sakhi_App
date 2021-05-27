0fdeb18509aa38be01154f8b60487feb252410e3
Manali <manalitanna29@gmail.com>
2021-05-27 11:10:24 +0530

    Added VolunteerPage

---
+++ sakhi/lib/screens/VolunteerPage/VolunteerPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sakhi/screens/VolunteerPage/body.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Patients',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: new Body(),
+    );
+  }
+}
