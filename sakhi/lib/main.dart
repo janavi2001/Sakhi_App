2bbf2fd518595f9c364504ec77845447216baebf
janavi <janavisrinivasan26@gmail.com>
2021-06-20 21:47:57 +0530

    tabs

--- sakhi/lib/main.dart
+++ sakhi/lib/main.dart
@@ -1,6 +1,6 @@
 //import 'dart:html';
 
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:sakhi/screens/VolunteerPage/VolunteerPage.dart';
 import 'package:sakhi/screens/DoctorPage/DoctorPage.dart';
 
@@ -78,5 +78,56 @@ class _LoginState extends State<LoginPage> {
         ),
       ),
     );
+  }
+}
+*/
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication_tutorial/authentication_service.dart';
+import 'package:firebase_authentication_tutorial/home_page.dart';
+import 'package:firebase_authentication_tutorial/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
   }
 }
